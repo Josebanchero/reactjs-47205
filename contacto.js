6276c5bf756010b4fae5b68d4fd2ac1df7ea19dd
Josebanchero <jose.banchero2@gmail.com>
2023-09-13 19:30:37 -0300

    primera entrega

---
+++ contacto.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
+const motivo = document.getElementById("motivo")
+const texto = document.getElementById("texto")
+
+
